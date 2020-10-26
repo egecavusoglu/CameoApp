939db9935e9c7bc4fee2bcdde73815e25954a173
Ege Cavusoglu <egecavusoglu99@gmail.com>
2020-10-26 16:32:53 +0300

    Navigation and movies screen layouts added.

---
+++ Cameo/DetailsVC.swift
@@ -0,0 +1,16 @@
+//
+//  DetailsVC.swift
+//  Cameo
+//
+//  Created by Ege Çavuşoğlu on 10/26/20.
+//  Copyright © 2020 Ege Çavuşoğlu. All rights reserved.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Cameo/FavoritesVC.swift
@@ -0,0 +1,15 @@
+//
+//  FavoritesVC.swift
+//  Cameo
+//
+//  Created by Ege Çavuşoğlu on 10/26/20.
+//  Copyright © 2020 Ege Çavuşoğlu. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
